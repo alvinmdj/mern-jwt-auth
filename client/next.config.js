27bb48d8d91490c9eb3e0fd13eb185045bb0c24a
AlvinMD <alvinmartindwrk@gmail.com>
2022-01-13 21:42:54 +0700

    add comment

--- client/next.config.js
+++ client/next.config.js
@@ -5,6 +5,8 @@ module.exports = {
       {
         source: '/api/:slug*',
         destination: 'https://next-jwt-auth.herokuapp.com/api/:slug*'
+        // this one for development:
+        // destination: 'http://localhost:5000/api/:slug*'
       }
     ]
   }
